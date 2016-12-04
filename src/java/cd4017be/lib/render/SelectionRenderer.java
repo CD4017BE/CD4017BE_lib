530219cbff93f561f8224b803d86f1dac86c0eb2
CD4017BE <cd4017be@gmail.com>
2016-12-04 23:41:56 +0100

    fix rendering glitches using GlStateManager.* instead of GL11.*

--- src/java/cd4017be/lib/render/SelectionRenderer.java
+++ src/java/cd4017be/lib/render/SelectionRenderer.java
@@ -1,14 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cd4017be.lib.render;
 
 import org.lwjgl.opengl.GL11;
 
 import cd4017be.api.automation.IOperatingArea;
 import cd4017be.lib.ModTileEntity;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.GlStateManager.DestFactor;
+import net.minecraft.client.renderer.GlStateManager.SourceFactor;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
@@ -21,26 +19,18 @@ import net.minecraftforge.fml.relauncher.SideOnly;
  * @author CD4017BE
  */
 @SideOnly(Side.CLIENT)
-public class SelectionRenderer extends TileEntitySpecialRenderer<ModTileEntity>
-{
-	
-	private void renderSelection(int[] area, double ofsX, double ofsY, double ofsZ)//TODO use GlStateManager
-	{
+public class SelectionRenderer extends TileEntitySpecialRenderer<ModTileEntity> {
+
+	private void renderSelection(int[] area, double ofsX, double ofsY, double ofsZ) {
 		if (area == null) return;
-		//save state
-		int Vbs = GL11.glGetInteger(GL11.GL_BLEND_SRC),
-				Vbd = GL11.glGetInteger(GL11.GL_BLEND_DST);
-		boolean Vc = GL11.glIsEnabled(GL11.GL_CULL_FACE),
-				Vl = GL11.glIsEnabled(GL11.GL_LIGHTING),
-				Vb = GL11.glIsEnabled(GL11.GL_BLEND);
 		//set state
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
-		GL11.glDisable(GL11.GL_CULL_FACE);
-		GL11.glDisable(GL11.GL_LIGHTING); 
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GL11.glDepthMask(false);
-		GL11.glColor4f(1, 1, 1, 1);
+		GlStateManager.bindTexture(0);
+		GlStateManager.disableCull();
+		GlStateManager.disableLighting(); 
+		GlStateManager.enableBlend();
+		GlStateManager.blendFunc(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA);
+		GlStateManager.depthMask(false);
+		GlStateManager.color(1, 1, 1, 1);
 		//render
 		int density = 0x40;
 		VertexBuffer t = Tessellator.getInstance().getBuffer();
@@ -79,13 +69,6 @@ public class SelectionRenderer extends TileEntitySpecialRenderer<ModTileEntity>
 		//done
 		t.setTranslation(0, 0, 0);
 		Tessellator.getInstance().draw();
-		//reset state
-		GL11.glDepthMask(true);
-		GL11.glBlendFunc(Vbs, Vbd);
-		if (!Vb) GL11.glDisable(GL11.GL_BLEND);
-		if (Vl) GL11.glEnable(GL11.GL_LIGHTING);
-		if (Vc) GL11.glEnable(GL11.GL_CULL_FACE);
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, Minecraft.getMinecraft().getTextureMapBlocks().getGlTextureId());
 	}
 
 	@Override
@@ -93,5 +76,5 @@ public class SelectionRenderer extends TileEntitySpecialRenderer<ModTileEntity>
 		if (te instanceof IOperatingArea && IOperatingArea.Handler.renderArea((IOperatingArea)te))
 			this.renderSelection(((IOperatingArea)te).getOperatingArea(), x - (double)te.getPos().getX(), y - (double)te.getPos().getY(), z - (double)te.getPos().getZ());
 	}
-	
+
 }
