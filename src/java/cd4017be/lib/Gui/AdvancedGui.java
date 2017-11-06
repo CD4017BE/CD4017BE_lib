2c91125d4cbc7bb39baf9118e8174725f7e0905b
CD4017BE <cd4017be@gmail.com>
2017-11-06 18:18:54 +0100

    fix GUIs not rendering background and item tool tips

--- src/java/cd4017be/lib/Gui/AdvancedGui.java
+++ src/java/cd4017be/lib/Gui/AdvancedGui.java
@@ -84,6 +84,12 @@ public abstract class AdvancedGui extends GuiContainer {
 		}
 	}
 
+	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+		this.drawDefaultBackground();
+		super.drawScreen(mouseX, mouseY, partialTicks);
+		this.renderHoveredToolTip(mouseX, mouseY);
+	}
+
 	@Override
 	protected void drawGuiContainerForegroundLayer(int mx, int my) {
 		GlStateManager.color(1, 1, 1, 1);
