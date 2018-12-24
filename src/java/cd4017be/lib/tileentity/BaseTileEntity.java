a0933285dbf064b91f47df1d13ff82805ebc73e5
CD4017BE <cd4017be@gmail.com>
2018-12-24 20:44:31 +0100

    implement a server side only version of ITickable

--- src/java/cd4017be/lib/tileentity/BaseTileEntity.java
+++ src/java/cd4017be/lib/tileentity/BaseTileEntity.java
@@ -14,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -87,6 +88,8 @@ public class BaseTileEntity extends TileEntity implements IAbstractTile {
 	@Override
 	public void onLoad() {
 		unloaded = false;
+		if (world.isRemote && this instanceof ITickableServerOnly)
+			world.tickableTileEntities.remove(this);
 		setupData();
 	}
 
@@ -162,5 +165,11 @@ public class BaseTileEntity extends TileEntity implements IAbstractTile {
 	public boolean isClient() {
 		return world.isRemote;
 	}
+
+	/**
+	 * Indicates that the implementing TileEntity should only receive server side update ticks.
+	 * @author CD4017BE
+	 */
+	public static interface ITickableServerOnly extends ITickable {}
 
 }
