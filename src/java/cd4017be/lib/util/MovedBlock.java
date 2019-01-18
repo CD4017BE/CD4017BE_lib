53c3a909aeee3b4b6a2153f3adfb471cf3e13dd2
CD4017BE <cd4017be@gmail.com>
2019-01-18 17:26:30 +0100

    fix CD4017BE/VerticallyStackedDimensions#1

--- src/java/cd4017be/lib/util/MovedBlock.java
+++ src/java/cd4017be/lib/util/MovedBlock.java
@@ -6,6 +6,7 @@ import cd4017be.api.automation.IOperatingArea;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
@@ -165,18 +166,16 @@ public class MovedBlock {
 	}
 
 	/**
-	 * Move an entity to a new Position
+	 * Move any entity to any new position
 	 * @param entity the Entity to move
 	 * @param dim new world dimension
 	 * @param x new x position
 	 * @param y new y position
 	 * @param z new z position
 	 */
-	public static void moveEntity(Entity entity, int dimN, double x, double y, double z) {
-		if (!entity.isEntityAlive() || entity.isRiding()) return;
-		WorldServer worldO = (WorldServer)entity.world;
-		int dimO = entity.dimension;
-		if (dimN == dimO) {
+	public static void moveEntity(Entity entity, int dim, double x, double y, double z) {
+		if (entity.isDead || entity.isRiding()) return;
+		if (dim == entity.dimension) {
 			if (entity instanceof EntityPlayerMP)
 				((EntityPlayerMP)entity).setPositionAndUpdate(x, y, z);
 			else
@@ -186,41 +185,100 @@ public class MovedBlock {
 		List<Entity> passengers = entity.getPassengers();
 		for (Entity e : passengers) {
 			e.dismountRidingEntity();
-			moveEntity(e, dimN, x, y, z);
+			moveEntity(e, dim, x, y, z);
 		}
-		MinecraftServer server = worldO.getMinecraftServer();
-		WorldServer worldN = server.getWorld(dimN);
-		if (entity instanceof EntityPlayerMP) {
-			PlayerList pl = server.getPlayerList();
-			EntityPlayerMP player = (EntityPlayerMP)entity;
-			player.dimension = dimN;
-			player.connection.sendPacket(new SPacketRespawn(player.dimension, worldN.getDifficulty(), worldN.getWorldInfo().getTerrainType(), player.interactionManager.getGameType()));
-			pl.updatePermissionLevel(player);
-			worldO.removeEntityDangerously(player);
-			transferEntityToWorld(player, worldN, x, y, z);
-			pl.preparePlayer(player, worldO);
-			player.connection.setPlayerLocation(player.posX, player.posY, player.posZ, player.rotationYaw, player.rotationPitch);
-			player.interactionManager.setWorld(worldN);
-			player.connection.sendPacket(new SPacketPlayerAbilities(player.capabilities));
-			pl.updateTimeAndWeatherForPlayer(player, worldN);
-			pl.syncPlayerInventory(player);
-			for (PotionEffect potioneffect : player.getActivePotionEffects())
-				player.connection.sendPacket(new SPacketEntityEffect(player.getEntityId(), potioneffect));
-			net.minecraftforge.fml.common.FMLCommonHandler.instance().firePlayerChangedDimensionEvent(player, dimO, dimN);
-		} else {
-			entity.dimension = dimN;
-			entity.world.removeEntity(entity);
-			transferEntityToWorld(entity, worldN, x, y, z);
-		}
+		
+		if (entity instanceof EntityPlayerMP)
+			entity = transferPlayer((EntityPlayerMP)entity, dim, x, y, z);
+		else
+			entity = transferEntity(entity, dim, x, y, z);
+		
 		for (Entity e : passengers) e.startRiding(entity, true);
 	}
 
-	private static void transferEntityToWorld(Entity entity, WorldServer world, double x, double y, double z) {
-		entity.isDead = false;
-		entity.setLocationAndAngles(x, y, z, entity.rotationYaw, entity.rotationPitch);
-		world.spawnEntity(entity);
-		world.updateEntityWithOptionalForce(entity, false);
-		entity.setWorld(world);
+	/**
+	 * Move a player to a new position in a <b>different</b> dimension
+	 * @param player the EntityPlayer to move
+	 * @param dimN new world dimension
+	 * @param x new x position
+	 * @param y new y position
+	 * @param z new z position
+	 * @return the resulting moved player
+	 */
+	public static EntityPlayerMP transferPlayer(EntityPlayerMP player, int dimN, double x, double y, double z) {
+		WorldServer worldO = (WorldServer)player.world;
+		MinecraftServer server = worldO.getMinecraftServer();
+		WorldServer worldN = server.getWorld(dimN);
+		PlayerList pl = server.getPlayerList();
+		int dimO = player.dimension;
+		
+		player.dimension = dimN;
+		player.connection.sendPacket(new SPacketRespawn(player.dimension, worldN.getDifficulty(), worldN.getWorldInfo().getTerrainType(), player.interactionManager.getGameType()));
+		pl.updatePermissionLevel(player);
+		worldO.removeEntityDangerously(player);
+		
+		player.isDead = false;
+		player.setLocationAndAngles(x, y, z, player.rotationYaw, player.rotationPitch);
+		worldN.spawnEntity(player);
+		worldN.updateEntityWithOptionalForce(player, false);
+		player.setWorld(worldN);
+		
+		pl.preparePlayer(player, worldO);
+		player.connection.setPlayerLocation(player.posX, player.posY, player.posZ, player.rotationYaw, player.rotationPitch);
+		player.interactionManager.setWorld(worldN);
+		player.connection.sendPacket(new SPacketPlayerAbilities(player.capabilities));
+		pl.updateTimeAndWeatherForPlayer(player, worldN);
+		pl.syncPlayerInventory(player);
+		for (PotionEffect potioneffect : player.getActivePotionEffects())
+			player.connection.sendPacket(new SPacketEntityEffect(player.getEntityId(), potioneffect));
+		net.minecraftforge.fml.common.FMLCommonHandler.instance().firePlayerChangedDimensionEvent(player, dimO, dimN);
+		return player;
+	}
+
+	/**
+	 * Move a non-player entity to a new position in a <b>different</b> dimension
+	 * @param entityO the original Entity to move
+	 * @param dimN new world dimension
+	 * @param x new x position
+	 * @param y new y position
+	 * @param z new z position
+	 * @return the resulting moved entity
+	 */
+	public static Entity transferEntity(Entity entityO, int dimN, double x, double y, double z) {
+		if (!net.minecraftforge.common.ForgeHooks.onTravelToDimension(entityO, dimN)) return null;
+		entityO.world.profiler.startSection("changeDimension");
+		entityO.dimension = dimN;
+		entityO.world.removeEntity(entityO);
+		entityO.isDead = false;
+		
+		entityO.world.profiler.startSection("reposition");
+		WorldServer worldO = (WorldServer)entityO.world;
+		entityO.setLocationAndAngles(x, y, z, entityO.rotationYaw, entityO.rotationPitch);
+		worldO.updateEntityWithOptionalForce(entityO, false);
+		
+		entityO.world.profiler.endStartSection("reloading");
+		MinecraftServer server = worldO.getMinecraftServer();
+		WorldServer worldN = server.getWorld(dimN);
+		Entity entityN = EntityList.newEntity(entityO.getClass(), worldN);
+		if (entityN != null) {
+			NBTTagCompound nbttagcompound = entityO.writeToNBT(new NBTTagCompound());
+			nbttagcompound.removeTag("Dimension");
+			entityN.readFromNBT(nbttagcompound);
+			
+			boolean flag = entityN.forceSpawn;
+			entityN.forceSpawn = true;
+			worldN.spawnEntity(entityN);
+			entityN.forceSpawn = flag;
+			worldN.updateEntityWithOptionalForce(entityN, false);
+		}
+		entityO.isDead = true;
+		entityO.world.profiler.endSection();
+		
+		worldO.resetUpdateEntityTick();
+		worldN.resetUpdateEntityTick();
+		entityO.world.profiler.endSection();
+		
+		return entityN;
 	}
 
 }
