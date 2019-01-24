55174c42aef0e8882069415337346ef85b3eae90
CD4017BE <cd4017be@gmail.com>
2019-01-24 16:30:07 +0100

    transfer pipe connection API from InductiveLogistics to CD4017BE_lib

---
+++ src/java/cd4017be/api/indlog/pipe/IItemPipeCon.java
@@ -0,0 +1,17 @@
+package cd4017be.api.indlog.pipe;
+
+import net.minecraft.util.EnumFacing;
+
+/**
+ * indicates that ItemPipes should automatically connect to the TileEntity implementing this
+ * @author CD4017BE
+ */
+public interface IItemPipeCon {
+
+	/**
+	 * @param s side to connect from
+	 * @return preferred transfer direction: 0 = none, 1 = in, 2 = out, 3 = both
+	 */
+	public byte getItemConnectDir(EnumFacing s);
+
+}
