52f2c6e104fec4fc1a34da1038908dec7f24b1dd
CD4017BE <cd4017be@gmail.com>
2019-07-14 21:20:18 +0200

    allow sensor mounts to handle block ref changes and have a sync state

--- src/java/cd4017be/api/rs_ctr/sensor/IBlockSensor.java
+++ src/java/cd4017be/api/rs_ctr/sensor/IBlockSensor.java
@@ -1,14 +1,17 @@
 package cd4017be.api.rs_ctr.sensor;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import cd4017be.api.rs_ctr.com.BlockReference;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.util.INBTSerializable;
 
 /**
  * Remote Comparator API for special sensor augments.<dl>
- * To add your own sensors simply write an implementation for this interface and register an instance of it alongside one or more ItemStacks using {@link SensorRegistry#register(java.util.function.Function, ItemStack...)}.
+ * To add your own sensors simply write an implementation for this interface and register an instance of it alongside one or more ItemStacks using {@link SensorRegistry#register(java.util.function.Function, ItemStack...)}.<br>
+ * Also implement {@link INBTSerializable} if this contains state to synchronize.
  * @author CD4017BE
  */
 public interface IBlockSensor {
@@ -18,7 +21,14 @@ public interface IBlockSensor {
 	 * @param block the block face to scan (chunk is guaranteed to be loaded).
 	 * @return a signal value for the given block
 	 */
-	int readValue(BlockReference block);
+	int readValue(@Nonnull BlockReference block);
+
+	/**
+	 * called when the BlockReference supplied to the comparator changes
+	 * @param block the new block
+	 * @return the actual block reference the sensor wants to interact with (usually the same as the given one).
+	 */
+	default BlockReference onRefChange(@Nullable BlockReference block, IHost host) { return block; }
 
 	/**
 	 * @return text displayed when aimed at
@@ -30,5 +40,16 @@ public interface IBlockSensor {
 	 * @see SensorRegistry#RENDERER
 	 */
 	@Nullable ResourceLocation getModel();
+
+	/**
+	 * The device hosting the sensor
+	 * @author CD4017BE
+	 */
+	public interface IHost {
+		/**
+		 * call this whenever the sensor's {@link INBTSerializable} state was modified.
+		 */
+		void syncSensorState();
+	}
 
 }
