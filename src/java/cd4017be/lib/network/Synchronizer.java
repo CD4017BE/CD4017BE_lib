e2086f768c0ba98dd5f22baafad0a8c4382ef548
CD4017BE <cd4017be@gmail.com>
2021-09-01 01:08:45 +0200

    fix index out of bounds

--- src/java/cd4017be/lib/network/Synchronizer.java
+++ src/java/cd4017be/lib/network/Synchronizer.java
@@ -202,7 +202,7 @@ public class Synchronizer<T> {
 		BitSet changes, int i0
 	) {
 		int i1 = i0 + indices.length, p0 = rawState.position();
-		for (int i = i0, j; i < i1 && (j = changes.nextSetBit(i)) >= 0; i++) {
+		for (int i = i0, j; i < i1 && (j = changes.nextSetBit(i)) >= 0 && j < i1; i++) {
 			if ((i = changes.nextClearBit(j + 1)) >= i1) i = i1 - 1;
 			rawState.limit(p0 + indices[i - i0]);
 			rawState.position(p0 + indices[j - i0]);
@@ -212,6 +212,7 @@ public class Synchronizer<T> {
 		i1--;
 		for (int i = changes.nextSetBit(i1); i >= 0; i = changes.nextSetBit(i + 1)) {
 			int j = i - i1;
+			if (j >= encoders.length) break;
 			encoders[j].encode(objState[j0 + j], pkt);
 		}
 	}
